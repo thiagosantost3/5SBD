33341bac26d336563d8c2385eb84017037d96aa4
thiagosantost3 <sonia.suzi@yahoo.com.br>
2023-12-15 19:18:58 -0300

    Adicionar arquivos de projeto.

---
+++ tabelas/Alunos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Alunos]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [nome] VARCHAR(50) NULL, 
+    [email] VARCHAR(50) NULL, 
+    [cpf] VARCHAR(50) NULL, 
+    [nascimento] VARCHAR(50) NULL
+)
